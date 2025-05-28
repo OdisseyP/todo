b47761d38d9592b1a9de178d955a1767c8ea0a80
OdisseyPogosov <pogosov.fortech@gmail.com>
2025-05-28 12:04:03 +0300

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { TodosService } from './todos.service';
+
+@Module({
+  imports: [TodosModule],
+  controllers: [AppController],
+  providers: [AppService, TodosService],
+})
+export class AppModule {}
