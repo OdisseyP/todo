20e2ef96c65a79aa67a1686a68576472d5f3d2d6
OdisseyPogosov <pogosov.fortech@gmail.com>
2025-05-29 15:33:48 +0300

    Подключил докер и TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,21 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { TodosService } from './todos/todos.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
     TodosModule,
   ],
 
