1790a7d6e2e740b3b7b16f3a875b4166f397e56e
OdisseyP <pogosov.fortech@gmail.com>
2025-06-03 16:27:48 +0300

    "add _"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { UsersService } from 'src/user/users.service';
 import { RegisterUserDto } from 'src/task/dto/register-user.dto';
 import { UserEntity } from 'src/user/user.entity';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,17 @@ export class AuthController {
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: RegisterUserDto })
+  @ApiResponse({
+    status: 201,
+    type: UserEntity,
+    description: 'User successfully registered',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'User with this email already exists',
+  })
   async register(
     @Body() registerUserDto: RegisterUserDto,
   ): Promise<Omit<UserEntity, 'password'>> {
